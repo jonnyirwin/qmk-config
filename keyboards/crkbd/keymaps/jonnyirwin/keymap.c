8c58cfad1eb290b19236839a3b75661437b3c2a9
Jonny Irwin <git@jbi.im>
2025-04-08 22:14:58 +0100

    updated corne keymap

--- keyboards/crkbd/keymaps/jonnyirwin/keymap.c
+++ keyboards/crkbd/keymaps/jonnyirwin/keymap.c
@@ -29,6 +29,7 @@ enum combos {
 	ALT_R,
 	ESC,
 	TAB,
+	FN
 };
 
 const uint16_t PROGMEM shift_l[] = {KC_P, KC_T, COMBO_END};
@@ -52,7 +53,7 @@ combo_t key_combos[COMBO_COUNT] = {
 	[ALT_L] = COMBO(alt_l, KC_LALT),
 	[ALT_R] = COMBO(alt_r, KC_LALT),
 	[ESC] = COMBO(esc, KC_ESC),
-	[TAB] = COMBO(tab, KC_TAB),
+	[TAB] = COMBO(tab, KC_TAB)
 };
 
 
@@ -60,58 +61,57 @@ enum layer_names {
     _CK = 0,
     _NAV,
     _SYM,
-    _MK
+		_FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_CK] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC ,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_DEL,
+      KC_ESC ,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_HOME,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,  KC_O,   KC_PGUP ,
+      KC_TAB,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,  KC_O,   KC_ENT ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_END,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_PGDN ,
+      CW_TOGG, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, C(KC_A) ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_BSPC, MO(_NAV), KC_SPC,     KC_ENT,   MO(_SYM), MO(_MK)
+                                        KC_BSPC, MO(_NAV), KC_SPC,     KC_ENT,   MO(_SYM), MO(_FN)
                                       //`--------------------------'  `--------------------------'
     ),
 
   [_NAV] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TRNS  ,    KC_1,    KC_2,    KC_3,    KC_4,  KC_5  ,                         KC_6,    KC_7,    KC_8,    KC_9,  KC_0  , KC_TRNS  ,
+      KC_TRNS  ,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5  ,                         KC_6,    KC_7,    KC_8,    KC_9,  KC_0  , KC_TRNS  ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS  , CAPS_WORD, KC_NO, KC_COMM, KC_DOT, KC_NO  ,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_NO  , KC_TRNS  ,
+      KC_TRNS  , CW_TOGG, KC_Z, C(KC_C), KC_V, C(KC_V)  ,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, G(C(KC_RIGHT))  , KC_TRNS  ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS , S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),                      S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_TRNS  ,
+      KC_TRNS , KC_NO, S(KC_1), S(KC_3), S(KC_5), KC_NO,												KC_NO, S(KC_7), S(KC_8), G(C(KC_LEFT)), KC_NO, KC_TRNS  ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_NO , KC_TRNS, KC_NO ,    KC_TAB , S(KC_INS) , KC_NO
+                                          KC_NO , KC_TRNS, KC_NO ,    KC_NO , KC_NO , KC_NO
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_SYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TRNS  ,S(KC_6),KC_LBRC,S(KC_2),KC_RBRC, S(KC_8) ,                      KC_NUHS,S(KC_EQL),KC_MINS,S(KC_MINS),S(KC_SLSH), KC_TRNS,
+      KC_TRNS  ,S(KC_6),KC_LBRC,S(KC_2),KC_RBRC, KC_NUHS ,                      KC_K,S(KC_EQL),KC_MINS,S(KC_MINS),S(KC_SLSH), KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS  , S(KC_4), S(KC_9), KC_QUOT, S(KC_0), S(KC_7),                   S(KC_NUHS), S(KC_COMM), KC_EQL, S(KC_DOT),S(KC_QUOT),KC_TRNS,
+      KC_TRNS  , S(KC_4), S(KC_9), KC_QUOT, S(KC_0), S(KC_NUBS),                   S(KC_NUHS), S(KC_COMM), KC_EQL, S(KC_DOT),S(KC_QUOT),KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS , S(KC_5) ,S(KC_LBRC),KC_GRV,S(KC_RBRC), S(KC_NUBS),                KC_NUBS,S(KC_1),KC_DOT,S(KC_3),KC_SLSH,KC_TRNS,
+      KC_TRNS , KC_NO ,S(KC_LBRC),KC_GRV,S(KC_RBRC), KC_NO,                KC_NO,KC_NUBS, KC_NO, KC_SLSH,KC_NO, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                    KC_CAPS_LOCK,C(KC_BSPC),KC_DEL,    KC_NO,KC_TRNS, KC_NO
+                                    KC_NO,C(KC_BSPC),KC_DEL,    KC_NO,KC_TRNS, KC_NO
                                       //`--------------------------'  `--------------------------'
   ),
 
-   [_MK] = LAYOUT_split_3x6_3(
+  [_FN] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TRNS  ,KC_NO ,KC_NO ,KC_MS_U ,KC_WH_U ,KC_ACL2 ,                      KC_NO  ,KC_NO ,KC_NO ,KC_NO ,KC_NO , KC_TRNS ,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS  ,KC_NO ,KC_MS_L ,KC_MS_D ,KC_MS_R ,KC_ACL1 ,                      KC_NO ,KC_BTN1 ,KC_BTN2 ,KC_BTN3 ,KC_NO,  KC_TRNS ,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS  ,KC_NO ,KC_NO ,KC_NO ,KC_WH_D ,KC_ACL0 ,                      KC_NO  ,KC_NO ,KC_NO ,KC_NO ,KC_NO , KC_TRNS ,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_NO  , KC_TRNS, KC_TRNS,    KC_NO, KC_NO,  KC_TRNS
-                                      //`--------------------------'  `--------------------------'
-  ),
-
+   KC_TRNS  ,KC_F1 ,KC_F2 ,KC_F3 ,KC_F4 ,KC_F5 ,                    KC_TRNS  ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,  KC_TRNS ,
+  // |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+   KC_TRNS  ,KC_F6 ,KC_F7 ,KC_F8 ,KC_F9 ,KC_F10 ,                    KC_TRNS  ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,  KC_TRNS ,
+  // |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+   KC_TRNS  ,KC_TRNS ,KC_F11 ,KC_F12 ,KC_NO ,KC_NO ,                    KC_TRNS  ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,  KC_TRNS ,
+  // |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         KC_NO  , KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,  KC_NO
+                                      // `--------------------------'  `--------------------------'
+  )
   // Blank layer template
   //[_LABEL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
