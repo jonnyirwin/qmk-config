c4ba3f98b7f907f83dad52674b8150a0f6239093
Jonny Irwin <git@jbi.im>
2026-03-17 22:34:57 +0000

    changed thumb keys as too reachy

--- keyboards/cheapino/keymaps/jonnyirwin/keymap.c
+++ keyboards/cheapino/keymaps/jonnyirwin/keymap.c
@@ -15,6 +15,42 @@ enum layer_names {
     _SYS,
 };
 
+/* Cheapino thumb override: SPC/ENT on inner (easiest) positions */
+#undef LAYER_CK_3x5_3
+#define LAYER_CK_3x5_3 \
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, \
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    \
+    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,      KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, \
+                  MO(_NAV), KC_SPC,  KC_BSPC,   C(KC_A), KC_ENT,   MO(_SYM)
+
+#undef LAYER_NAV_3x5_3
+#define LAYER_NAV_3x5_3 \
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
+    CW_TOGG, KC_Z,    C(KC_C), KC_V,    C(KC_V),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, G(C(KC_RGHT)), \
+    KC_NO,   S(KC_1), S(KC_3), S(KC_5), KC_NO,     KC_NO,   S(KC_7), S(KC_8), G(C(KC_LEFT)), KC_NO, \
+                  KC_TRNS, KC_NO,    KC_NO,     KC_NO,   KC_NO,   KC_NO
+
+#undef LAYER_SYM_3x5_3
+#define LAYER_SYM_3x5_3 \
+    S(KC_6), KC_LBRC, S(KC_2), KC_RBRC, KC_NUHS,   KC_K,    S(KC_EQL),KC_MINS,S(KC_MINS),S(KC_SLSH), \
+    S(KC_4), S(KC_9), KC_QUOT, S(KC_0), S(KC_NUBS), S(KC_NUHS),S(KC_COMM),KC_EQL,S(KC_DOT),S(KC_2), \
+    KC_NO,   S(KC_LBRC),KC_GRV,S(KC_RBRC),KC_NO,   KC_NO,   KC_NUBS, KC_NO,   KC_SLSH, KC_NO,  \
+                  KC_NO, C(KC_BSPC), KC_DEL,    KC_NO,   KC_NO,   KC_TRNS
+
+#undef LAYER_FN_3x5_3
+#define LAYER_FN_3x5_3 \
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
+    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
+    KC_NO,   KC_F11,  KC_F12,  KC_F13,  KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
+                  KC_NO,   KC_NO,    KC_NO,     KC_NO,   KC_NO,   KC_NO
+
+#undef LAYER_SYS_3x5_3
+#define LAYER_SYS_3x5_3 \
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
+    QK_REBOOT,QK_BOOT,KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   QK_BOOT, QK_REBOOT, \
+                  KC_TRNS, KC_NO,    KC_NO,     KC_NO,   KC_NO,    KC_TRNS
+
 enum combos {
     SHIFT_L,
     SHIFT_R,
@@ -24,7 +60,6 @@ enum combos {
     GUI_R,
     ALT_L,
     ALT_R,
-    BKSPC,
     SYS_LYR,
     FN_LYR,
     ESC,
@@ -40,12 +75,11 @@ const uint16_t PROGMEM gui_l[]   = {KC_W,    KC_R,     COMBO_END};
 const uint16_t PROGMEM gui_r[]   = {KC_Y,    KC_I,     COMBO_END};
 const uint16_t PROGMEM alt_l[]   = {KC_Q,    KC_A,     COMBO_END};
 const uint16_t PROGMEM alt_r[]   = {KC_SCLN, KC_O,     COMBO_END};
-const uint16_t PROGMEM bkspc[]   = {MO(_NAV),KC_SPC,   COMBO_END};
 const uint16_t PROGMEM sys_lyr[] = {MO(_NAV),MO(_SYM), COMBO_END};
 const uint16_t PROGMEM fn_lyr[]  = {KC_K,    KC_SLSH,  COMBO_END};
 const uint16_t PROGMEM esc[]     = {KC_J,    KC_M,     COMBO_END};
 const uint16_t PROGMEM tab[]     = {KC_B,    KC_G,     COMBO_END};
-const uint16_t PROGMEM tmux[]    = {KC_ENT,  MO(_SYM), COMBO_END};
+const uint16_t PROGMEM tmux[]    = {MO(_SYM), KC_ENT,   COMBO_END};
 
 combo_t key_combos[] = {
     [SHIFT_L] = COMBO(shift_l, KC_LSFT),
@@ -56,7 +90,6 @@ combo_t key_combos[] = {
     [GUI_R]   = COMBO(gui_r,   KC_LGUI),
     [ALT_L]   = COMBO(alt_l,   KC_LALT),
     [ALT_R]   = COMBO(alt_r,   KC_LALT),
-    [BKSPC]   = COMBO(bkspc,   KC_BSPC),
     [SYS_LYR] = COMBO(sys_lyr, MO(_SYS)),
     [FN_LYR]  = COMBO(fn_lyr,  MO(_FN)),
     [ESC]     = COMBO(esc,     KC_ESC),
